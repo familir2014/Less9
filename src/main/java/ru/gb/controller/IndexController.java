6c6a33400e6a27d1736a9c33c9af6071a010d4ab
Pavel <familir2014@mail.ru>
2021-12-18 18:35:24 +0300

    HW9

---
+++ src/main/java/ru/gb/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ru.gb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    @RequestMapping
+    public String indexPage() {
+        return "index";
+    }
+}
